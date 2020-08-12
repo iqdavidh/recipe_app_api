09278a30bdd79d3b86b302c3de2d291f2d5516dd
davidh <iq.davidh@gmail.com>
2020-08-12 03:33:08 -0500

    ya tenemos el modelo de user

--- app/core/models.py
+++ app/core/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
+        # Normalizar el email para ser case sensitive
+        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -15,3 +15,22 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_create_user_email_normalize(self):
+        """Los correos se gaurdan con el dominio normalizado - pero el usaurio no se toca"""
+        email = "dummy@Test.com"
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='no se ocupa en el test'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+        email2 = "Dummy@test.com"
+        user2 = get_user_model().objects.create_user(
+            email=email2,
+            password='no se ocupa en el test'
+        )
+
+        self.assertEqual(user2.email, email2)
